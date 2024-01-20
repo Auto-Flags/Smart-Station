5ef1bcb7dd8c3f2010df01a718abe27250740e54
ebrahim969 <ebrahimelnemr084@gmail.com>
2024-01-20 17:34:27 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartStation());
+}
+
+class SmartStation extends StatelessWidget {
+  const SmartStation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
