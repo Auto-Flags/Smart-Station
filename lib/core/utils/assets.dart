e6b74a4d4066a6ec50a36cfa4b7560ae71e08e89
ebrahim969 <ebrahimelnemr084@gmail.com>
2024-01-20 20:14:35 +0200

    add utils and images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -4,5 +4,21 @@ class Assets {
   /// Assets for imagesAppIcon
   /// assets/images/app_icon.png
   static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesAppOnBoarding1
+  /// assets/images/app_on_boarding_1.svg
+  static const String imagesAppOnBoarding1 = "assets/images/app_on_boarding_1.svg";
+
+  /// Assets for imagesAppOnBoarding2
+  /// assets/images/app_on_boarding_2.svg
+  static const String imagesAppOnBoarding2 = "assets/images/app_on_boarding_2.svg";
+
+  /// Assets for imagesAppOnBoarding3
+  /// assets/images/app_on_boarding_3.svg
+  static const String imagesAppOnBoarding3 = "assets/images/app_on_boarding_3.svg";
+
+  /// Assets for imagesAppOnBoarding3Icon
+  /// assets/images/app_on_boarding_3_icon.png
+  static const String imagesAppOnBoarding3Icon = "assets/images/app_on_boarding_3_icon.png";
 }
 
---
+++ lib/core/utils/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles
+{
+  static const changaBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Changa',
+    fontWeight: FontWeight.bold
+  );
+}
