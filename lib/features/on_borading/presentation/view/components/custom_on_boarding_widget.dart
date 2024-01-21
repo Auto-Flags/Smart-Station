9d5c4c3f2d475059acca72d33c4a7d450b5181b8
ebrahim969 <ebrahimelnemr084@gmail.com>
2024-01-21 02:34:05 +0200

    add flutter localization

--- lib/features/on_borading/presentation/view/components/custom_on_boarding_widget.dart
+++ lib/features/on_borading/presentation/view/components/custom_on_boarding_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smart_station/core/utils/color.dart';
-import 'package:smart_station/core/utils/style.dart';
 import 'package:smart_station/features/on_borading/data/model/on_boarding_model.dart';
 import 'package:smart_station/features/on_borading/presentation/view/components/on_boarding_widget_item1.dart';
 import 'package:smart_station/features/on_borading/presentation/view/components/on_boarding_widget_item2.dart';
